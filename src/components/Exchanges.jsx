4beb70717f2d552671d1e8a0d1cf04903034f436
Arka Sheikh <skarka90@gmail.com>
2023-04-16 15:45:06 +0530

    Done before graph

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Exchanges() {
+  return <div>Exchange</div>;
+}
+
+export default Exchanges;
---
+++ src/components/LineChart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
+
+const { Title } = Typography;
+
+function LineChart({ coinHistory, currentPrice, coinName }) {
+  console.log(coinHistory, currentPrice, coinName);
+  return <div>fsvbgsgvsvsfv</div>;
+}
+
+export default LineChart;
